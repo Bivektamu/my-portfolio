0b17cfe7b385f501798d734a40790991f4b4ba8d
Bivek Gurung <bivek.tamu@gmail.com>
2026-05-17 21:33:36 +1000

    chore:fix production bugs

--- src/components/Blob.jsx
+++ src/components/Blob.jsx
@@ -1,18 +1,10 @@
-import React from 'react'
 import { BlobDiv } from "../styles/blobStyles.js"
-import { motion, useScroll, useTransform } from 'motion/react'
 
 const Blob = () => {
-    const {scrollY} = useScroll()
-
-    const yText = useTransform(scrollY, [0, 600], [0, -150]);
-    const yImage = useTransform(scrollY, [0, 600], [0, 150]);
+   
   return (
 
-    // <motion.div
-    //   style={{ y: yText }}>
-    //   <BlobDiv id='blob' className='scale'></BlobDiv>
-    // </motion.div>
+   
       <BlobDiv id='blob' className='scale'></BlobDiv>
 
   )
